75e8d66e45ad3445daccebcfafa08f64b76d2aec
danzy-ux <muhammadzidan0097@gmail.com>
2026-01-23 09:39:42 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,20 @@
 const CACHE_NAME = "warung-zidan-v1";
-const FILES_TO_CACHE = ["./","./index.html"];
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
