d3f54728d036c458b9699e8717d7b24ee735851d
danzy-ux <muhammadzidan0097@gmail.com>
2026-01-23 09:29:34 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "warung-zidan-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
